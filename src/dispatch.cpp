ef5b1e00291e224964466f07e3fe179ebf416235
Alexandr Mansurov <alexander.mansurov@gmail.com>
2015-02-13 15:09:37 +0100

    styling

--- src/dispatch.cpp
+++ src/dispatch.cpp
@@ -4,111 +4,100 @@
 // Edmonds.cpp : Defines the entry point for the console application.
 //
 
-#include "./edmonds.h"
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <memory>
+#include <string>
 
-// using namespace nsp;
+#include "./edmonds.h"
 
-/*int _tmain(int argc, _TCHAR* argv[])
-{
-	Graph g,m;
-	if(MappingFinder::step(g,m)){
-		//v m je parovani
-	}
-
-	return 0;
-}*/
+using nsp::Graph;
 
 void usage() {
-	std::cout << "Usage: edmonds <filename>" << std::endl;
+  std::cout << "Usage: edmonds <filename>" << std::endl;
 }
 
 std::unique_ptr<nsp::Graph> parse(const std::string & file) {
-	std::cout << "Parsing file " << file << std::endl;
-	std::ifstream f;
-	f.open(file.c_str(), std::ios::in);
-	if (! f.is_open()) {
-		f.close();
-		std::cout << "Failed to open file" << std::endl;
-		return nullptr;
-	} else {
-		int edges = -1;
-		int vertices = -1;
+  std::cout << "Parsing file " << file << std::endl;
+  std::ifstream f;
+  f.open(file.c_str(), std::ios::in);
+  if (!f.is_open()) {
+    f.close();
+    std::cout << "Failed to open file" << std::endl;
+    return nullptr;
+  } else {
+    int edges = -1;
+    int vertices = -1;
 
-		std::string line;
-		getline(f,line);
-		std::istringstream iss(line);
-		iss >> edges;
-		iss >> vertices;
-		if ((edges < 0) || (vertices < 0)) {
-			std::cout<<"Format error"<<std::endl;
-			f.close();
-			return nullptr;
-		} else {
-			//vytvorit graf
-			using nsp::Graph;
-			std::unique_ptr<Graph> g(new Graph(vertices, edges));
-			for (int i = 0; i < edges; i++) {
-				getline(f,line);
-				std::istringstream s2(line);
-				s2.str(line);
-				int x = -1;
-				int y = -1;
-				s2 >> x;
-				s2 >> y;
+    std::string line;
+    getline(f, line);
+    std::istringstream iss(line);
+    iss >> edges;
+    iss >> vertices;
+    if ((edges < 0) || (vertices < 0)) {
+      std::cout << "Format error" << std::endl;
+      f.close();
+      return nullptr;
+    } else {
+      // vytvorit graf
+      std::unique_ptr<Graph> g(new Graph(vertices, edges));
+      for (int i = 0; i < edges; i++) {
+        getline(f, line);
+        std::istringstream s2(line);
+        s2.str(line);
+        int x = -1;
+        int y = -1;
+        s2 >> x;
+        s2 >> y;
 
-				if ((x>=0) && (x<vertices) && (y>=0) && (y<vertices)) {
-					if (x != y) {
-						if (!(*g).setEdge(x,y)) {
-							std::cout << "Edge error ("<<x<<","<<y<<")"<<std::endl;
-							return nullptr;
-						}
-					} else {
-						std::cout << "Forbidden edge ("<<x<<","<<y<<")"<<std::endl;
-						return nullptr;
-					}
-				} else {
-					std::cout << "Range error for edge ("<<x<<","<<y<<")"<<std::endl;
-					return nullptr;
-				}
-			}
-			f.close();
-			return g;
-			//graph = g.get();
-		}
-	}
+        if ((x >= 0) && (x < vertices) && (y >= 0) && (y < vertices)) {
+          if (x != y) {
+            if (!(*g).setEdge(x, y)) {
+              std::cout << "Edge error (" << x << "," << y << ")" << std::endl;
+              return nullptr;
+            }
+          } else {
+            std::cout << "Forbidden edge (" <<x<< "," << y << ")" << std::endl;
+            return nullptr;
+          }
+        } else {
+          std::cout << "Range error for edge (" << x << "," << y << ")"
+            << std::endl;
+          return nullptr;
+        }
+      }
+      f.close();
+      return g;
+    }
+  }
 }
 
-int main(int argc, char** argv)
-{
-	//nacti parametry
-	if (argc <= 1	) {
-		usage();
-		return 1;
-	}
+int main(int argc, char** argv) {
+  // nacti parametry
+  if (argc <= 1) {
+    usage();
+    return 1;
+  }
 
-	//naparsuj soubor, ziskej pointer na graf
-	std::string file = argv[1];
-	std::unique_ptr<nsp::Graph> g = parse(file);
-	if (g == nullptr) {
-		std::cout << "fail" << std::endl;
-		return 1;
-	}
-	//potvrd korektnost
-	g->print();
-	std::cout<<"Continue? [y/n]"<<std::endl;
-	char c = 'x';
-	std::cin >> c;
-	if ((c == 'y') || (c == 'Y')) {
-		//pust hledac
-		std::unique_ptr<nsp::Graph> m = nsp::MappingFinder::FindMaxMapping(g.get());
-		(*m).print();
-	}
- 	return 0;
-
+  // naparsuj soubor, ziskej pointer na graf
+  std::string file = argv[1];
+  std::unique_ptr<nsp::Graph> g = parse(file);
+  if (g == nullptr) {
+    std::cout << "fail" << std::endl;
+    return 1;
+  }
+  // potvrd korektnost
+  g->print();
+  std::cout << "Continue? [y/n]" << std::endl;
+  char c = 'x';
+  std::cin >> c;
+  if ((c == 'y') || (c == 'Y')) {
+    // pust hledac
+    std::unique_ptr<nsp::Graph> m = nsp::MappingFinder::FindMaxMapping(g.get());
+    (*m).print();
+  }
+  return 0;
 }
 
 
